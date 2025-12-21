ee5e3d8261c27785509a51ab66fb969917f8c704
Filip Nowakowski <f.nowakowski2000@gmail.com>
2025-12-21 19:54:53 +0100

    1.0.0-alpha1

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+## 1.0.0
+
+- Wold's Vaults 0.25.2
+- Initial release
---
+++ kubejs/server_scripts/woldsvaults/wold_star.js
@@ -0,0 +1,24 @@
+onEvent('recipes', event => {
+    // event.shaped(Item.of('woldsvaults:wold_star'),
+    //     [
+    //         'XXX',
+    //         'XRX',
+    //         'XXX'
+    //     ], {
+    //     X: 'woldsvaults:wold_star_chunk',
+    //     R: 'the_vault:omega_pog'
+    // })
+
+    // event.shaped(Item.of('the_vault:trinket_scrap'),
+    //     [
+    //         'XVX',
+    //         'VRV',
+    //         'XVX'
+    //     ], {
+    //     X: 'woldsvaults:wold_star_chunk',
+    //     V: 'the_vault:gem_pog',
+    //     R: 'the_vault:perfect_echo_gem'
+    // })
+
+    //event.shapeless('the_vault:unidentified_relic_fragment', ['woldsvaults:wold_star_chunk', 'the_vault:vault_relic_fragment'])
+})
