3a01a5c359cf956ba61fd1263ebf2b50e747cac0
Filip Nowakowski <f.nowakowski2000@gmail.com>
2026-01-16 15:00:30 +0100

    feat: updated recipes and addons

---
+++ kubejs/server_scripts/minecraft_the_server/wolsvaults/recipes.js
@@ -0,0 +1,6 @@
+onEvent("recipes", event => {
+    event.shapeless(`woldsvaults:wold_star`, [
+        'woldsvaults:wold_star_chunk',
+        'incorporeal:octuple_compressed_tiny_potato'
+    ])
+})
