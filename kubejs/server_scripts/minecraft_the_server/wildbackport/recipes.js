61b1396c08696a26be22844a49f8fa9b7865c555
Filip Nowakowski <f.nowakowski2000@gmail.com>
2026-01-28 16:19:38 +0100

    feat: mud recipes

---
+++ kubejs/server_scripts/minecraft_the_server/wildbackport/recipes.js
@@ -0,0 +1,9 @@
+onEvent("recipes", event => {
+    event.shapeless('buildscape:mud', ['wildbackport:mud'])
+    event.shapeless('wildbackport:mud', ['buildscape:mud'])
+
+    event.recipes.createFilling(`wildbackport:mud`, [
+        'minecraft:dirt',
+        Fluid.of(`minecraft:water`, 100)
+    ])
+})
