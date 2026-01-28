b3850a2aef5a47aaf68a417036a7a38e29e50a99
Filip Nowakowski <f.nowakowski2000@gmail.com>
2026-01-28 16:35:18 +0100

    1.11.0 (#56)
    * fix: remove farsight
    
    * feat: mud recipes
    
    * feat: soap recipes
    
    * feat: roleplay clothes mod
    
    * chore: bcc-common
    
    * chore: changelog
    
    * chore: changelog fix
    
    * chore: link fix

---
+++ kubejs/server_scripts/minecraft_the_server/moa_decor_bath/recipes.js
@@ -0,0 +1,6 @@
+onEvent("recipes", event => {
+    event.stonecutting('moa_decor_bath:jabon', 'supplementaries:soap')
+    event.stonecutting('moa_decor_bath:soporteparajabon', 'supplementaries:soap')
+    event.stonecutting('moa_decor_bath:jabonera', 'supplementaries:soap')
+    event.stonecutting('moa_decor_bath:jaboneradepared', 'supplementaries:soap')
+})
